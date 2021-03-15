a549e88f80aa4628b5b524bafabfb597ce6265a4
KawinM23 <kawin.m235@gmail.com>
2021-03-15 20:11:28 +0700

    Draft 1

--- src/model/Fish.java
+++ src/model/Fish.java
@@ -4,5 +4,9 @@ package model;
 public abstract class Fish {
 	protected Position position;
 	
+	public Fish() {
+		
+	}
 	
+	public abstract void Move();
 }
---
+++ src/model/Goldfish.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Goldfish extends Fish {
+	
+	public Goldfish() {
+		super();
+	}
+	
+	@Override
+	public void Move() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
