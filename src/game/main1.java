02d021e52a8550c182a7728b909b3d39139ee084
KawinM23 <kawin.m235@gmail.com>
2021-04-21 16:42:57 +0700

    Rewrite Base

---
+++ src/game/main1.java
@@ -0,0 +1,10 @@
+package game;
+
+public class main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/model/base/Unit.java
@@ -0,0 +1,5 @@
+package model.base;
+
+public class Unit {
+
+}
