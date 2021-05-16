a0bda927698ba8b8260f80c2a87d76d6807555dd
KawinM23 <kawin.m235@gmail.com>
2021-05-17 00:04:25 +0700

    LevelManager and Level

---
+++ src/manager/Level.java
@@ -0,0 +1,16 @@
+package manager;
+
+import java.util.ArrayList;
+
+import model.base.Fish;
+
+public class Level {
+	//Level,StartMoney,StartFish,ShopDetail,InvasionDetail
+	private String mode;
+	private int[] id;
+	
+	private int startingMoney;
+	private ArrayList<Fish> startingFish;
+	
+	
+}
--- src/manager/LevelManager.java
+++ src/manager/LevelManager.java
@@ -4,4 +4,6 @@ public class LevelManager {
 	//TODO
 	//Store Each Level Information
 	//Level,StartMoney,StartFish,ShopDetail,InvasionDetail
+	
+	
 }
