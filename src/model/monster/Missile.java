e965071cc6f6b8c55b60241ffef3aa967c9f2770
KawinM23 <kawin.m235@gmail.com>
2021-05-20 17:06:35 +0700

    Add Gus and Destructor

---
+++ src/model/monster/Missile.java
@@ -0,0 +1,44 @@
+package model.monster;
+
+import javafx.scene.canvas.GraphicsContext;
+import model.base.Monster;
+import properties.Renderable;
+
+public class Missile extends Monster implements Renderable{
+
+	public Missile(String name, double posX, double posY) {
+		super(name, posX, posY);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void attack() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void getHit() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void continuePause(long duration) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(int fr) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(GraphicsContext gc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
