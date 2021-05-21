672549e2acc51c1fb1691aa21f16a2d07ef45284
Tuchtham Sungkameka <touch0869@gmail.com>
2021-05-21 13:31:38 +0700

    Added Select Level Option in menu

--- src/template/menu1.java
+++ src/template/menu1.java
@@ -25,7 +25,7 @@ public class menu1 {
 	// { XOfTopLeft, YOfTopLeft, XOfBottomRight, YOfBottomRight, borderRadius,
 	// FontSize }
 	final double[][] buttonDetail = { { 354.0, 43.0, 576.0, 116.0, 100.0, 50.0 },
-			{ 354.0, 138.0, 576.0, 192.0, 30.0, 40.0 }, { 354.0, 207.0, 576.0, 262.0, 30.0, 40.0 },
+			{ 354.0, 135.0, 576.0, 189.0, 30.0, 38.0 }, { 354.0, 207.0, 576.0, 262.0, 30.0, 40.0 },
 			{ 354.0, 282.0, 576.0, 357.0, 100.0, 30.0 }, { 402.0, 377.0, 529.0, 409.0, 80.0, 20.0 },
 			{ 323.0, 410.0, 417.0, 443.0, 78.0, 15.0 }, { 416.0, 410.0, 512.0, 443.0, 10.0, 15.0 },
 			{ 513.0, 410.0, 603.0, 443.0, 80.0, 20.0 } };
@@ -110,6 +110,25 @@ public class menu1 {
 						+ "-fx-background-color: transparent;" + "-fx-text-fill: moccasin");
 				SoundManager.playClickSound();
 				if (buttonText.equals("Start")) {
+					Thread thread = new Thread(() -> {
+						try {
+							Platform.runLater(new Runnable() {
+								@Override
+								public void run() {
+									// TODO Auto-generated method stub
+									SceneController.changeScene("TankAll");
+								}
+							});
+
+						} catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+
+					});
+					thread.start();
+
+				} else if (buttonText.equals("Select Level")) {
 					Thread thread = new Thread(() -> {
 						try {
 							Platform.runLater(new Runnable() {
